7871efea1aa588f0475319b7db5e37b51ca64c35
Wumberly <kjdunne321@gmail.com>
2023-11-24 19:23:18 +0000

    fibonacci level picker function

--- GrammarGame/grammar.js
+++ GrammarGame/grammar.js
@@ -1,5 +1,6 @@
 
-import { fetchCSVData, getRandomRow } from './grammarFunctions.js';
+import { fetchCSVData } from './grammarFunctions.js';
+import { getRandomRow } from './fibonacci.js';
 
 // Get references to DOM elements
 // Navbar elements
@@ -38,7 +39,7 @@ fetchCSVData("grammar.csv")
   .then(dataArray => {
     if (dataArray) {
       dataRows = dataArray;
-      const randomRow = getRandomRow(dataRows, 5);
+      const randomRow = getRandomRow(dataRows, 3);
       console.log('Random Row:', randomRow);
     } else {
       console.error('Failed to fetch CSV data.');
@@ -50,8 +51,32 @@ homeLink.addEventListener('click', function () {
   window.location.href = '../Homepage/index.html';
 });
 
+
+let levelsArray = [];
 startButton.addEventListener('click', function () {
-  console.log('Start button clicked');
+    console.log('Start button clicked');
+
+    // Get a random row
+    const currentRow = getRandomRow(dataRows, 5);
+
+    // Check if a valid row is returned
+    if (currentRow !== null) {
+        // Extract the level from the current row
+        const level = parseInt(currentRow.Level);
+
+        // Push the level to the array
+        levelsArray.push(level);
+
+
+        // Display the count of each level
+        const levelCounts = Array.from({ length: 5 }, (_, i) =>
+            levelsArray.filter(l => l === i + 1).length
+        );
+
+        console.log('Level Counts:', levelCounts);
+    } else {
+        console.error('No valid row returned.');
+    }
 });
 
 checkButton.addEventListener('click', function () {
