dc27ffe17adf00e9b5d3ad86216e111e88596f3d
Wumberly <kjdunne321@gmail.com>
2023-11-30 12:54:19 +0000

    Update to noew homepage

---
+++ home.js
@@ -0,0 +1,43 @@
+document.addEventListener('DOMContentLoaded', function () {
+    // Sample data for the chart
+    const chartData = {
+        labels: ['Vocabulary', 'Grammar', 'Overall'],
+        datasets: [{
+            label: 'Performance',
+            data: [30, 50, 40],
+            backgroundColor: ['#426C94', '#426C94','#FF5733'],
+            borderWidth: 2,
+        }]
+    };
+
+    // Get the correct chart canvas element
+    const chartCanvas = document.getElementById('performance-chart').getContext('2d');
+
+    // Create the chart with adjusted bar width
+    const performanceChart = new Chart(chartCanvas, {
+        type: 'bar',
+        data: chartData,
+        options: {
+            scales: {
+                y: {
+                    beginAtZero: true,
+                    max: 100,
+                }
+            },
+            plugins: {
+                legend: {
+                    display: false
+                }
+            },
+            layout: {
+                padding: {
+                    left: 10,
+                    right: 10,
+                    top: 10,
+                    bottom: 10
+                }
+            },
+            barPercentage: 0.6,
+        }
+    });
+});
